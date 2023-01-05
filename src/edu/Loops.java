d4c7f10ac2d992f0c8f6eb32ae686c258a94c200
nishant-dongare <nishantdongare@outlook.com>
2023-01-05 12:41:16 +0530

    factorial and fibonacci

--- src/edu/Loops.java
+++ src/edu/Loops.java
@@ -9,10 +9,122 @@ public class Loops {
 //		whileLoop();
 //		doWhileLoop();
 //		doWhileinfinite();
-		whileinfinite();
+//		whileinfinite();
+//		sumOfAllDigits();
+//		reverse();
+//		stringReverse();
+//		fibonacci();
+//		factorial();
+//		perfectSquare();
+//		swapWithoutTemp();
+		perfectSquare();
 	}
 
-	private static void whileinfinite() {
+	private static void swapWithoutTemp() {
+		int a = 10, b = 20;
+		a = a + b;
+		b = a - b;
+		a = a - b;
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+	private static void perfectSquare() {
+		int sqr = 4;
+		int num = 0, n = sqr;
+		int i = 2;
+		while (num != 1) {
+			if (n % i == 0) {
+				n = sqr / i;
+				num += n;
+				i++;
+			}
+
+		}
+		System.out.println(num);
+
+	}
+
+	private static void factorial() {
+		int n = 5;
+		int fact = 1;
+		for (int i = 1; i <= n; i++) {
+			fact = fact * i;
+		}
+		System.out.println("FOR : " + fact);
+
+		// While loop
+		int i = 1;
+		fact = 1;
+		while (i <= n) {
+			fact = fact * i;
+			i++;
+		}
+		System.out.println("WHILE : " + fact);
+	}
+
+	private static void fibonacci() {
+		int n1 = 0, n2 = 1, sum = 0;
+		System.out.println(n1 + "\n" + n2);
+		for (int i = 0; i < 5; i++) {
+			sum = n1 + n2;
+			System.out.println(sum);
+			n1 = n2;
+			n2 = sum;
+		}
+	}
+
+	private static void stringReverse() {
+		String s = "Nishant";
+		String str = "";
+		int len = s.length() - 1;
+		char c;
+		while (len != 0) {
+			c = s.charAt(len);
+			str = str + c;
+			len--;
+		}
+		System.out.println(str);
+
+		// using toCharArray
+		String asd = "Hello";
+		char[] charArray = asd.toCharArray();
+		String rev = "";
+		System.out.println(charArray);
+		int length = charArray.length - 1;
+		for (int i = charArray.length - 1; i >= 0; i--) {
+			rev = rev + charArray[i];
+		}
+		System.out.println(rev);
+	}
+
+	private static void reverse() {
+		int rev = 0, rem;
+		int num = 789;
+		while (num != 0) {
+			rem = num % 10;
+			rev = rev * 10 + rem;
+			num = num / 10;
+		}
+		System.out.println(rev);
+
+	}
+
+	private static void sumOfAllDigits() {
+		int sum = 0, rem;
+		int num = 46;
+		while (num != 0) {
+			rem = num % 10;
+			sum += rem;
+			num = num / 10;
+		}
+		System.out.println(sum);
+
+	}
+
+	private static void whileInfinite() {
+
 		int a = 10, b = 20;
 		boolean loop = true;
 		Scanner sc = new Scanner(System.in);
@@ -39,6 +151,7 @@ public class Loops {
 				loop = false;
 			}
 		}
+		sc.close();
 
 	}
 
@@ -68,7 +181,7 @@ public class Loops {
 		}
 	}
 
-	private static void doWhileinfinite() {
+	private static void doWhileInfinite() {
 		boolean loop = true;
 		Scanner sc = new Scanner(System.in);
 		do {
